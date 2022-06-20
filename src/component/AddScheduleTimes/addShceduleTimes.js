904590f8c8156e5f7ac2f2779552795f5a7f458e
Manuel <105855842+Gurrupato@users.noreply.github.com>
2022-06-20 20:14:07 +0200

    divided

---
+++ src/component/AddScheduleTimes/addShceduleTimes.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+const mapValues = (fn, obj) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value)])
+  );
+
+const addCourseTimes = (course) => ({
+  ...course,
+  ...timeParts(course.meets),
+});
+
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
+
+export const addScheduleTimes = (schedule) => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses),
+});
---
+++ src/component/Term/TermSelector.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+export const terms = { F: "Fall", W: "Winter", S: "Spring", Sm: "Summer" };
+
+const TermButton = ({ term, setTerm, checked }) => (
+  <>
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      checked={checked}
+      autoComplete="off"
+      onChange={() => setTerm(term)}
+    />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
+);
+
+export const TermSelector = ({ term, setTerm }) => (
+  <div className="btn-group">
+    {Object.values(terms).map((value) => (
+      <TermButton
+        key={value}
+        term={value}
+        setTerm={setTerm}
+        checked={value === term}
+      />
+    ))}
+  </div>
+);
